4ce094e33f656519f96e1894e16c4362a6b7b602
ritesh-khandekar-mystiqlabs <ritesh.mystiqlabs@gmail.com>
2023-10-28 21:01:19 +0530

    collectstatic

--- classifier/classifier/settings.py
+++ classifier/classifier/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'classifier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            "backend/templates/"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
