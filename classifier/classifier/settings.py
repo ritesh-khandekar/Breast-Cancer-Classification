08ca9a57d1bd4b3a73672efb1b43e9bc62ff0b6c
ritesh-khandekar-mystiqlabs <ritesh.mystiqlabs@gmail.com>
2023-10-28 19:34:25 +0530

    added db settings

--- classifier/classifier/settings.py
+++ classifier/classifier/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'classifier.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
